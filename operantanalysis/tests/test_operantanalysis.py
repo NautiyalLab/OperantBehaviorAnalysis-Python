18f0b2b4f9f0ae7a722e7cab933156b2f66cf5e8
ssdesroc <43478912+ssdesroc@users.noreply.github.com>
2019-02-26 22:06:08 -0500

    Update test_operantanalysis.py

--- operantanalysis/tests/test_operantanalysis.py
+++ operantanalysis/tests/test_operantanalysis.py
@@ -1,7 +1,8 @@
 from ..operantanalysis import accessfiles, rewardretrieval, respondingduringcueanditi, leverpressing
 import os.path as op
 
-data_path = op.join(operantanalysis.__path__[0], 'sampledata')
+data_path = op.join(..operantanalysis.__path__[0], 'sampledata')
+
 
 def test_accessfiles():
     (subjectnumber, timecode, eventcode) = accessfiles(data_path)
