68dc216fb3051c77d3a33e1f305e4756a2d3faa6
ssdesroc <43478912+ssdesroc@users.noreply.github.com>
2019-02-27 10:46:36 -0500

    Update test_operantanalysis.py

--- operantanalysis/tests/test_operantanalysis.py
+++ operantanalysis/tests/test_operantanalysis.py
@@ -1,4 +1,4 @@
-from ..operantanalysis import accessfiles, rewardretrieval, respondingduringcueanditi, leverpressing
+from operantanalysis import accessfiles, rewardretrieval, respondingduringcueanditi, leverpressing
 
 
 def test_accessfiles():
