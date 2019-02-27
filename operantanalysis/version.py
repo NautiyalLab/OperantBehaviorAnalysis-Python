a5e01fe2c223645d8589bc0647980c241154b886
ssdesroc <43478912+ssdesroc@users.noreply.github.com>
2019-02-26 19:43:08 -0500

    Update version.py

--- operantanalysis/version.py
+++ operantanalysis/version.py
@@ -62,5 +62,5 @@ MAJOR = _version_major
 MINOR = _version_minor
 MICRO = _version_micro
 VERSION = __version__
-PACKAGE_DATA = {'operantanalysis': [pjoin('sanpledata', '*')]}
+PACKAGE_DATA = {'operantanalysis': [pjoin('sampledata', '*')]}
 REQUIRES = 
