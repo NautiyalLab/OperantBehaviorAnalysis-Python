0f6dd522c0568de2792c305474b4fdda2752d22d
ssdesroc <43478912+ssdesroc@users.noreply.github.com>
2019-02-26 22:47:39 -0500

    Update test_operantanalysis.py
    made pep 8 compliant

--- operantanalysis/tests/test_operantanalysis.py
+++ operantanalysis/tests/test_operantanalysis.py
@@ -1,5 +1,5 @@
 from ..operantanalysis import accessfiles, rewardretrieval, respondingduringcueanditi, leverpressing
-import os.path as op
+
 
 def test_accessfiles():
     (subjectnumber, timecode, eventcode) = accessfiles("../operantanalysis/sampledata/!2018-11-27_08h39m.Subject _0001.txt")
