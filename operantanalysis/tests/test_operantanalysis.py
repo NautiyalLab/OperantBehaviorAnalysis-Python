a8a13b4caf36825f2d6f1f23c92744039622b672
ssdesroc <43478912+ssdesroc@users.noreply.github.com>
2019-02-26 21:30:51 -0500

    Update test_operantanalysis.py

--- operantanalysis/tests/test_operantanalysis.py
+++ operantanalysis/tests/test_operantanalysis.py
@@ -1,8 +1,8 @@
-from operantanalysis import *
+import operantanalysis as oa
 
 
 def test_accessfiles():
-    (subjectnumber, timecode, eventcode) = accessfiles(
+    (subjectnumber, timecode, eventcode) = oa.accessfiles(
         "../sampledata/!2018-11-27_08h39m.Subject _0001.txt")
     assert len(timecode) == len(eventcode)
     for i in timecode:
@@ -12,19 +12,19 @@ def test_accessfiles():
 
 
 def test_rewardretrieval():
-    assert rewardretrieval([0, 1, 2, 3, 4], [1011, 25, 26, 1001, 1011]) == (1, 1, 0)
-    assert rewardretrieval([0, 1, 2, 3, 4], [25, 1011, 26, 1001, 1011]) == (1, 1, 1)
-    assert rewardretrieval([0, 1, 2, 3, 4, 5, 6], [25, 1011, 1001, 1011, 26, 1001, 1011]) == (1, 1, 1)
-    assert rewardretrieval([0, 1, 2, 3, 4, 5, 6, 7], [1011, 25, 26, 25, 26, 1001, 1011, 1001]) == (2, 2, 0)
+    assert oa.rewardretrieval([0, 1, 2, 3, 4], [1011, 25, 26, 1001, 1011]) == (1, 1, 0)
+    assert oa.rewardretrieval([0, 1, 2, 3, 4], [25, 1011, 26, 1001, 1011]) == (1, 1, 1)
+    assert oa.rewardretrieval([0, 1, 2, 3, 4, 5, 6], [25, 1011, 1001, 1011, 26, 1001, 1011]) == (1, 1, 1)
+    assert oa.rewardretrieval([0, 1, 2, 3, 4, 5, 6, 7], [1011, 25, 26, 25, 26, 1001, 1011, 1001]) == (2, 2, 0)
 
 
 def test_respondingduringcueanditi():
-    assert respondingduringcueanditi([0, 1, 2, 3, 4, 5], [113, 1011, 1, 1011, 2, 1011], 1, 2) == (30.0, 30.0)
-    assert respondingduringcueanditi([0, 1, 2, 3, 4, 5], [113, 1, 1011, 1011, 2, 1011], 1, 2) == (40.0, 0.0)
-    assert respondingduringcueanditi([0, 1, 2, 3, 4, 5], [113, 1011, 1011, 1, 2, 1011], 1, 2) == (0.0, 60.0)
+    assert oa.respondingduringcueanditi([0, 1, 2, 3, 4, 5], [113, 1011, 1, 1011, 2, 1011], 1, 2) == (30.0, 30.0)
+    assert oa.respondingduringcueanditi([0, 1, 2, 3, 4, 5], [113, 1, 1011, 1011, 2, 1011], 1, 2) == (40.0, 0.0)
+    assert oa.respondingduringcueanditi([0, 1, 2, 3, 4, 5], [113, 1011, 1011, 1, 2, 1011], 1, 2) == (0.0, 60.0)
 
     
 def test_leverpressing():
-    assert leverpressing([113, 1011, 1, 1011, 2, 1011], 1, 2) == (1, 1, 2)
-    assert leverpressing([113, 1, 1011, 1011, 2, 1011], 1) == (1, 0, 1)
-    assert leverpressing([113, 1011, 1011, 1, 2, 2, 1011], 1, 2) == (1, 2, 3)
+    assert oa.leverpressing([113, 1011, 1, 1011, 2, 1011], 1, 2) == (1, 1, 2)
+    assert oa.leverpressing([113, 1, 1011, 1011, 2, 1011], 1) == (1, 0, 1)
+    assert oa.leverpressing([113, 1011, 1011, 1, 2, 2, 1011], 1, 2) == (1, 2, 3)
