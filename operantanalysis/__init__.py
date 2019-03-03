f4ffba85b91d5b9f04def9e4c6119b96670a38fd
ssdesroc <43478912+ssdesroc@users.noreply.github.com>
2019-03-03 15:16:51 -0500

    Update __init__.py
    Added eventcodes_dictionary

--- operantanalysis/__init__.py
+++ operantanalysis/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import, division, print_function
 from .version import __version__  # noqa
 from .operantanalysis import * # noqa
+from .eventcodes import eventcodes_dictionary
