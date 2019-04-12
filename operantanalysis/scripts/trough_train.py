140d0b07b5eef26a8ee175123941d6b407981011
ssdesroc <desrochers.stephanie@gmail.com>
2019-04-12 09:21:11 -0400

    Update trough_train.py
    let plotting decide limits for latency graph

--- operantanalysis/scripts/trough_train.py
+++ operantanalysis/scripts/trough_train.py
@@ -33,7 +33,7 @@ group_means['Dippers Retrieved'].plot(legend=True, yerr=group_sems['Dippers Retr
 plt.ylabel('Dippers Retrieved')
 
 plt.subplot(122)
-group_means['Retrieval Latency'].plot(legend=True, yerr=group_sems['Retrieval Latency'], ylim=[0, 20], xlim=[0, days + 1],
+group_means['Retrieval Latency'].plot(legend=True, yerr=group_sems['Retrieval Latency'], xlim=[0, days + 1],
                                       xticks=(range(1, days + 1, 1)), marker='o', capsize=3, elinewidth=1)
 plt.ylabel('Retrieval Latency (sec)')
 plt.show()
