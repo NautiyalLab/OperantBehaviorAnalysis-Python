50c7fbd86929b62ee6b54bf165833e09bf643d52
ssdesroc <desrochers.stephanie@gmail.com>
2019-09-25 13:09:37 -0400

    Update lickometer_bouts.py
    Added code for lick rate during bouts

--- operantanalysis/scripts/lickometer_bouts.py
+++ operantanalysis/scripts/lickometer_bouts.py
@@ -15,18 +15,28 @@ for file in sorted(glob.glob(file_pattern)):
 df.rename(columns=df.iloc[0]).drop(df.index[0])
 
 df2 = pd.DataFrame()
+df3 = pd.DataFrame()
 
 for colname, col in df.iteritems():
     bout_list = []
+    rate_list = []
     bout = 0
+    lick_count = 0
     for x in col:
         if x <= 1000:
             bout += x
+            lick_count += 1
         elif x > 1000:
             bout_list += [bout]
+            if bout > 0:
+                rate_list += [lick_count / bout]
             bout = 0
     dftemp = pd.DataFrame(np.array(bout_list))
     df2 = pd.concat([df2, dftemp], ignore_index=True, axis=1)
+    dftemp2 = pd.DataFrame(np.array(rate_list))
+    df3 = pd.concat([df3, dftemp2], ignore_index=True, axis=1)
 
 df2.columns = df.columns
-df2.to_excel("output.xlsx")
+df3.columns = df.columns
+df2.to_excel("boutlength.xlsx")
+df3.to_excel("lickrate.xlsx")
