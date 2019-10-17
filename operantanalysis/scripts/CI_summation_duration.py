0cab8bc3ed425c66b99bdb2aa89c5a8d50c3f388
ssdesroc <desrochers.stephanie@gmail.com>
2019-10-17 11:12:24 -0400

    Update CI_summation_duration.py
    fixed code for experimental group

--- operantanalysis/scripts/CI_summation_duration.py
+++ operantanalysis/scripts/CI_summation_duration.py
@@ -15,7 +15,7 @@ def CI_summation_function(loaded_file, i):
     (timecode, eventcode) = extract_info_from_file(loaded_file, 500)
     (B_dur_individual, B_dur_total, BITI_dur_individual, BITI_dur_total) = cue_responding_duration(timecode, eventcode, 'ExcitorBTrialStart', 'ExcitorBTrialEnd', 'PokeOn1', 'PokeOff1')
     (BX_dur_individual, BX_dur_total, BXITI_dur_individual, BXITI_dur_total) = cue_responding_duration(timecode, eventcode, 'InhibitorTrialStart', 'InhibitorTrialEnd', 'PokeOn1', 'PokeOff1')
-    df2 = pd.DataFrame([[loaded_file['Subject'], loaded_file['CI'], int(i + 1), float(B_dur_total),
+    df2 = pd.DataFrame([[loaded_file['Subject'], loaded_file['MSN'], int(i + 1), float(B_dur_total),
                          float(BITI_dur_total), float(BX_dur_total), float(BXITI_dur_total)]], columns=column_list)
 
     return df2
