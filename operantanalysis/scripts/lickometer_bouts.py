884f53e81ebd9b5eed785a9c8e7df403be293620
ssdesroc <desrochers.stephanie@gmail.com>
2019-10-24 16:06:24 -0400

    Update lickometer_bouts.py

--- operantanalysis/scripts/lickometer_bouts.py
+++ operantanalysis/scripts/lickometer_bouts.py
@@ -5,6 +5,7 @@ from tkinter import filedialog
 from tkinter import *  # noqa
 import pandas as pd
 import math
+import statistics
 
 root = Tk()  # noqa
 root.withdraw()
@@ -17,6 +18,7 @@ df.rename(columns=df.iloc[0]).drop(df.index[0])
 print(df)
 df2 = pd.DataFrame()
 df3 = pd.DataFrame()
+df4means = pd.DataFrame()
 
 for colname, col in df.iteritems():
     bout_list = []
@@ -51,8 +53,13 @@ for colname, col in df.iteritems():
     df2 = pd.concat([df2, dftemp], ignore_index=True, axis=1)
     dftemp2 = pd.DataFrame(np.array(rate_list))
     df3 = pd.concat([df3, dftemp2], ignore_index=True, axis=1)
+    dftempmeans = pd.DataFrame(np.array([len(col), len(rate_list), sum(bout_list)/len(rate_list), sum(rate_list)/len(rate_list)]))
+    df4means = pd.concat([df4means, dftempmeans], ignore_index=True, axis=1)
 
 df2.columns = df.columns
 df3.columns = df.columns
+df4means.columns = df.columns
 df2.to_excel("boutlength.xlsx")
 df3.to_excel("lickrate.xlsx")
+df4means.to_excel("summarydata.xlsx")
+
