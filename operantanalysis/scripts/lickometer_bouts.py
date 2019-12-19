99717ba935f4490cf49c202c2e859ac63250f7c5
ssdesroc <desrochers.stephanie@gmail.com>
2019-12-19 17:21:32 -0500

    Update lickometer_bouts.py
    Added lick rate for first two minutes, output in summary data

--- operantanalysis/scripts/lickometer_bouts.py
+++ operantanalysis/scripts/lickometer_bouts.py
@@ -27,6 +27,8 @@ for colname, col in df.iteritems():
     lick_count = 0
     x2 = 0
     nan = 0
+    totaltime = -col[0]
+    lick_count_2 = 0
     for x in col:
         x2 += 1
         if x <= 1000:
@@ -49,17 +51,21 @@ for colname, col in df.iteritems():
             if bout > 0:
                 rate_list += [lick_count / bout]
             bout = 0
+        totaltime += x
+        if totaltime <= 1200:
+            lick_count_2 += 1
     if len(rate_list) == 0:
         mean_bout_l = 0
         mean_rate = 0
     else:
         mean_bout_l = sum(bout_list)/len(rate_list)
         mean_rate = sum(rate_list)/len(rate_list)
+    lick_rate_2min = lick_count_2 / 1200
     dftemp = pd.DataFrame(np.array(bout_list))
     df2 = pd.concat([df2, dftemp], ignore_index=True, axis=1)
     dftemp2 = pd.DataFrame(np.array(rate_list))
     df3 = pd.concat([df3, dftemp2], ignore_index=True, axis=1)
-    dftempmeans = pd.DataFrame(np.array([np.count_nonzero(~np.isnan(col)), len(rate_list), mean_bout_l, mean_rate]))
+    dftempmeans = pd.DataFrame(np.array([np.count_nonzero(~np.isnan(col)), len(rate_list), mean_bout_l, mean_rate, lick_rate_2min]))
     df4means = pd.concat([df4means, dftempmeans], ignore_index=True, axis=1)
 
 df2.columns = df.columns
@@ -67,5 +73,4 @@ df3.columns = df.columns
 df4means.columns = df.columns
 df2.to_excel("boutlength.xlsx")
 df3.to_excel("lickrate.xlsx")
-df4means.to_excel("summarydata.xlsx")
-
+df4means.to_excel("summarydata.xlsx") # rows are total licks, total bouts, mean bout length, mean rate of licking during bouts, lick rate in first two minutes after first lick
