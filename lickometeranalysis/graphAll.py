a31c64e5e77813073be7c665d9fb1a9cf442eb7a
mspring-dartmouth <mitchell.spring@dartmouth.edu>
2021-04-14 12:49:36 -0400

    Added GUI functionality for selecting directory containing data files.

--- lickometeranalysis/graphAll.py
+++ lickometeranalysis/graphAll.py
@@ -4,51 +4,73 @@ import LickometerAnalysisKit
 import glob
 from sys import argv
 from os import path
+from tkinter import filedialog
+from tkinter import *  # noqa
 
 ##### PREAMBLE #####
 
-# Handling of user arguments to avoid errors based on ordering
-user_args = argv
-
-# Remove script name from list of arguments. 
+# Safely remove script name from list of arguments . 
+user_args = argv.copy()
 user_args.pop(0)
 
-# Check for arguments pertaining to preferences for graphing
-if 'use_defaults' in user_args:
-	drop_first_block = True
-	norm_by_water = True
-	grouping_criteria = 'CONCENTRATION'
-	user_args.remove('use_defaults')
+
+# If the user has provided arguments, process them.
+if len(user_args) != 0:
+    # Check for arguments pertaining to preferences for graphing
+    if 'use_first_block' in user_args:
+        drop_first_block = False
+        user_args.remove('use_first_block')
+        print('Including first presentation block in averages.')
+    else:
+        drop_first_block = True
+        print('Excluding first presentation block from averages.')
+    if 'no_normalization' in user_args:
+        norm_by_water = False
+        user_args.remove('no_normalization')
+        print('Using raw lick counts for averages.')
+    else:
+        norm_by_water = True
+        print('Normalizing lick counts to water consumption.')
+    if 'group_by_TUBE' in user_args:
+        grouping_criteria = 'TUBE'
+        user_args.remove('group_by_TUBE')
+    else:
+        grouping_criteria = 'CONCENTRATION'
+    print(f'Grouping by {grouping_criteria}')
+
+    # With all other possible arguments removed, check for a path to files
+    try:
+        path_to_files = user_args[0]
+    except IndexError:
+        print('User did not provide path to files.')
+        path_to_files = input('Please enter it now. (Press enter to prompt GUI).    ')
+
+    # If no path provided, use a GUI prompt.
+    if path_to_files == '':
+        root = Tk()  # noqa
+        root.withdraw()
+        path_to_files = filedialog.askdirectory()
+# If no user input was provided, prompt for all variables. 
 else:
-	if 'drop_first_block' in user_args:
-		drop_first_block = True
-		user_args.remove('drop_first_block')
-	if 'norm_by_water' in user_args:
-		norm_by_water = True
-		user_args.remove('drop_first_block')
+    print('Please select a directory containing files for analysis.')
+    root = Tk()  # noqa
+    root.withdraw()
+    path_to_files = filedialog.askdirectory()
+    norm_by_water = bool(input('Normalize licking by water intake (True/False):    '))
+    drop_first_block = bool(input('Drop first block of presentations from analysis (True/False):    '))
+    grouping_criteria = input('Enter grouping criteria (Concentration/Tube):    ').upper()
 
-	if 'group_by_TUBE' in user_args:
-		grouping_criteria = 'TUBE'
-		user_args.remove('group_by_TUBE')
-	else:
-		grouping_criteria = 'CONCENTRATION'
-
-# With all other possible arguments removed, check for a path to files
-try:
-	path_to_files = user_args[0]
-except IndexError:
-	print('User did not provide path to files.')
-	path_to_files = input('Please enter it now. (Press enter to prompt GUI).    ')
-
-if path_to_files == '':
-	#TODO: Add Tk interface to select directory path.
 
 
 # Pull the name of the directory containing files to use as 
-base_folder_name = os.path.basename(path_to_files)
+base_folder_name = path.basename(path_to_files)
 
-target_file_search_term = os.path.join(path_to_files, '*.txt')
+target_file_search_term = path.join(path_to_files, '*.txt')
 files = glob.glob(target_file_search_term)
+
+if len(files) == 0:
+    print(f'{target_file_search_term} yielded no files. Try again.')
+    raise RuntimeError
 
 
 ##### EXECUTION #####
@@ -58,18 +80,18 @@ files = glob.glob(target_file_search_term)
 df_dict, ili_dict, animals = LickometerAnalysisKit.read_raw_files(files)
 # Create DataFrames
 total_df, total_df_by_animals = LickometerAnalysisKit.gen_summary_dataframe(df_dict, animals, base_folder_name,
-																		    grouping_criteria=grouping_criteria)
+                                                                            group_by_concentration=bool(grouping_criteria=='CONCENTRATION'))
 
 # Make graphs.
 
 LickometerAnalysisKit.graph_cumulative_licks(total_df_by_animals, animals, base_folder_name)
 
 LickometerAnalysisKit.individual_graph_by_group(total_df, animals, base_folder_name, 
-												grouping_criteria=grouping_criteria, 
-												drop_first_block=drop_first_block, 
-												normalize_by_water_consumption=norm_by_water)
+                                                grouping_criteria=grouping_criteria, 
+                                                drop_first_block=drop_first_block, 
+                                                normalize_by_water_consumption=norm_by_water)
 
 LickometerAnalysisKit.summarize_licking_curve(total_df, base_folder_name, 
-												grouping_criteria=grouping_criteria, 
-												drop_first_block=drop_first_block, 
-												normalize_by_water_consumption=norm_by_water)
+                                                grouping_criteria=grouping_criteria, 
+                                                drop_first_block=drop_first_block, 
+                                                normalize_by_water_consumption=norm_by_water)
